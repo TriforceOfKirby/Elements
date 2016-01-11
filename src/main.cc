1a750ec13d9b18467ce7219b4161d8bf7c47cca0
TriforceOfKirby <TriforceOfKirby@gmail.com>
2016-01-11 15:27:54 -0600

    Added entity creation test

--- src/main.cc
+++ src/main.cc
@@ -1,8 +1,13 @@
+#include <iostream>
 #include <memory>
-#include "weapon.hh"
 #include "armor.hh"
+#include "entity.hh"
+#include "weapon.hh"
 
 int main() {
+	auto player = std::make_unique<Entity>("TriforceOfKirby");
 	auto sword = std::make_unique<Weapon>("Iron Sword");
 	auto chestplate = std::make_unique<Armor>("Iron Chestplate");
+
+	std::cout << player->Strength() << std::endl;
 }
